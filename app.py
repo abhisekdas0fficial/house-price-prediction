22b2d0a7727ac4ed3039875bfbeb6c6e26410ffb
Abhisek Das <dasabhisek1999@gmail.com>
2022-06-12 01:27:17 +0530

    modified

--- app.py
+++ app.py
@@ -2,19 +2,37 @@ import pandas as pd
 import streamlit as st
 import pickle
 
+LL = pd.read_csv("LL.csv")
+
 st.set_page_config(page_title="House Price Prediction App", page_icon="🏠")
 st.title("House Price Prediction App")
 
 with open ("model.pkl", "rb") as f:
     model = pickle.load(f)
 
-def prediction(model, X_test):
-    y_pred = model.predict(X_test)
-    return y_pred
 
-x = st.number_input("Enter the total number of rooms in the house: ")
+location = st.sidebar.selectbox("Select Location", LL.location.unique())
+bhk = st.number_input(
+        "Total number of bedrooms in the house: ",
+        min_value=1,
+        max_value=8,
+        value=2
+    )
+bath = st.number_input(
+        "Total number of bathrooms in the house: " ,
+        min_value=1,
+        max_value=8,
+        value=2
+    )
+
+total_sqft = st.slider("Total Square Feet", 800, 25000, value=1500, step=100)
 
 if st.button("Predict"):
-    X_test = pd.DataFrame({"x": [x]})
-    y_pred = prediction(model, X_test)
-    st.success(f"The price of the house is {y_pred[0]:.2f}")
+    hdf = pd.DataFrame({
+        "location"   : location,
+        "total_sqft" : total_sqft,
+        "bath"       : bath,
+        "bhk"        : bhk
+    }, index=[1])
+    y_pred = model.predict(hdf)
+    st.write(f"# ₹{y_pred[0]:.2f} lakhs")
